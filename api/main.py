b10d3ce8a94e59eba7eec80daeeb97a90269268a
Kangfu00 <kemmatat323@gmail.com>
2025-11-01 14:00:05 +0700

    good

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from app.schemas import WordResponse
+from fastapi import HTTPException
+from app.routers import words
 
 # Initialize FastAPI app
 app = FastAPI(
@@ -7,16 +10,19 @@ app = FastAPI(
     description="API for vocabulary practice and learning"
 )
 
-@app.get("/api/word")
+app.include_router(words.router, prefix='/api', tags='words')
+'''
+@app.get("/api/word", response_model=WordResponse)
 def get_random_word():
     """Get a random word"""
     # TODO Write logic here....
-    return {
-        "word": "example",
-        "definition": "a representative form or pattern",
-        "difficulty_level": "Beginner"
-    }
-
+    return WordResponse(
+        id=12,
+        word="book",
+        definition="Reading material for sleep",
+        difficulty_level="Beginner"
+    )
+'''
 @app.get("/")
 def read_root():
     return {
